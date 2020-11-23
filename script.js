4344cee035b02b677fc7cad5ce7a7fc910b47519
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2020-11-23 16:05:58 -0600

    breed

--- script.js
+++ script.js
@@ -3,64 +3,71 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 
-function getDogImage(numberOfDogs) {
-  console.log(numberOfDogs);
-  let baseUrl = 'https://dog.ceo/api/breeds/image/random/';
-  fetch(`${baseUrl}${numberOfDogs}`)
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
-}
+// function getDogImage(numberOfDogs) {
+//   console.log(numberOfDogs);
+//   let baseUrl = 'https://dog.ceo/api/breeds/image/random/';
+//   fetch(`${baseUrl}${numberOfDogs}`)
+//     .then(response => response.json())
+//     .then(responseJson =>
+//       displayResults(responseJson))
+//     .catch(error => alert('Something went wrong. Try again later.'));
+// }
 
-function getDogBreed(breedOfDog){
+function getDogBreed(breedOfDog) {
   let baseUrl = 'https://dog.ceo/api/breed/';
   fetch(`${baseUrl}${breedOfDog}/images`)
-    .then(response => response.json())
-    .then(responseJson =>
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson))
+    .catch((error) => alert('Something went wrong. Try again later.'));
 }
 
+// function displayResults(responseJson) {
+//   console.log(responseJson);
+//   $('.result-list').empty();
+//   console.log(responseJson.message.length);
+//   if (responseJson.message.length > 0){
+//     for (let i = 0; i < responseJson.message.length; i++){
+//       console.log('read');
+//       $('.result-list').append(
+//         `<li><img src="${responseJson.message[i]}" class="results-img"></li>`
+//       );
+//     }
+//   }else {
+//     $('.result-list').append('<li> No Results</li>');
+//   }
+//   $('.results').removeClass('hidden');
+// }
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.result-list').empty();
-  console.log(responseJson.message.length);
-  if (responseJson.message.length > 0){
-    for (let i = 0; i < responseJson.message.length; i++){
-      console.log('read');
-      //console.log (`"url" : ${responseJson[i]}`)
-      $('.result-list').append(
-        `<li><img src="${responseJson.message[i]}" class="results-img"></li>`
-      );
-    }
-  }else {
-    $('.result-list').append('<li> No Results</li>');
+  let html = '';
+  html = `<img src="${responseJson.message}" class="results-img"/>`;
+  if (responseJson.status === 'error') {
+    alert('Something went wrong. Try again later.');
+  } else {
+    //display the results section
+    $('.results').replaceWith(html);
+    $('.results').removeClass('hidden');
   }
-  //display the results section
-  $('.results').removeClass('hidden');
 }
 
-
-function watchForm() { 
-  $('.js-search-form').submit(event => { 
+function watchForm() {
+  $('.js-search-form').submit((event) => {
     // console.log('here');
-    event.preventDefault();  
+    event.preventDefault();
     let newInput = $('#dogs').val();
-    console.log(newInput);   
-    getDogImage(newInput);   
-  });  
+    console.log(newInput);
+    getDogImage(newInput);
+  });
 
-  $('.js-breed-form').submit(event => {
+  $('.js-breed-form').submit((event) => {
     event.preventDefault();
     let newBreed = $('#breed').val();
     getDogBreed(newBreed);
   });
 }
 
-$(function() {
+$(function () {
   console.log('App loaded!');
   watchForm();
-}); 
-
+});
